0dcf7b90531ebe7af8acbdd07c84901851d3ad0c
Dhruv Patel <darup67@gmail.com>
2019-04-04 18:52:11 -0400

    clicky-game

---
+++ my-app/src/components/score/score.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./score.css";
+import Arrow from 'react-icons/lib/fa/caret-right';
+
+//stateless component
+const Score = props => ( < div className = "gameScore" >
+    <
+    h3 className = "score" > Your Score < Arrow / > { props.total } < /h3> <
+    h4 className = "status" > { props.status } < /h4> </div >
+);
+
+export default Score;
