df24d8957cf8914658ad7ddf0daadf2a567ea5fa
Dhruv Patel <darup67@gmail.com>
2019-04-04 19:12:38 -0400

    clicky-game

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
-import Card from "./components/Card";
-import Wrapper from "./components/Wrapper";
-import Score from "./components/Score";
-import pups from "./cards.json";
+import wrapper from "./components/wrapper";
+import card from "./components/card";
+import score from "./components/score";
+import superheroes from "./cards.json";
 import "./App.css";
 
 class App extends Component {
     // Setting this.state.pups to the cards json array
     state = {
-        pups,
+        superheroes,
         clickedPuppyIds: [],
         score: 0,
         goal: 8,
@@ -17,25 +17,25 @@ class App extends Component {
 
     //shuffle the pup cards in the browser when clicked
     shuffleScoreCard = id => {
-        let clickedPuppyIds = this.state.clickedPuppyIds;
+        let SuperherosClickIds = this.state.SuperherosClickIds;
 
-        if (clickedPuppyIds.includes(id)) {
-            this.setState({ clickedPuppyIds: [], score: 0, status: "Game Over! You lost. Click to play again!" });
+        if (SuperherosClickIds.includes(id)) {
+            this.setState({ SuperherosClickIds: [], score: 0, status: "Game Over! You lost. Click to play again!" });
             return;
         } else {
             clickedPuppyIds.push(id)
 
-            if (clickedPuppyIds.length === 8) {
-                this.setState({ score: 8, status: "You Won! Great Job, Smartie! Click to play again!", clickedPuppyIds: [] });
-                console.log('You Win');
+            if (SuperherosClickIds.length === 8) {
+                this.setState({ score: 8, status: "You Won! Click to play again!", SuperherosClickIds: [] });
+                console.log('You Win!');
                 return;
             }
 
-            this.setState({ pups, clickedPuppyIds, score: clickedPuppyIds.length, status: " " });
+            this.setState({ superheroes, SuperherosClickIds score: SuperherosClickIds.length, status: " " });
 
-            for (let i = pups.length - 1; i > 0; i--) {
+            for (let i = superheroes.length - 1; i > 0; i--) {
                 let j = Math.floor(Math.random() * (i + 1));
-                [pups[i], pups[j]] = [pups[j], pups[i]];
+                [superheroes[i], superheroes[j]] = [superheroes[j], superheroes[i]];
             }
         }
     }
@@ -51,8 +51,8 @@ class App extends Component {
             p className = "App-intro" >
             Try not to click the same image twice!
             <
-            /p> <
-            /header> <
+            /p> < /
+            header > <
             Score total = { this.state.score }
             goal = { 8 }
             status = { this.state.status }
@@ -73,8 +73,8 @@ class App extends Component {
             target = "_blank"
             rel = "noopener noreferrer" > here < /a>.</p >
             <
-            /footer> <
-            /div>
+            /footer> < /
+            div >
         );
     }
 }
